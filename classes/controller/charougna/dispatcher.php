b5d349e29b4e944d838215b40df27ba5a6e77ea9
mtou <mtou@charougna.com>
2011-06-14 22:54:40 +0200

    Initial commit (empty Dispatcher and Charougna_Dispatcher controllers)

---
+++ classes/controller/charougna/dispatcher.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Declares Charougna_Dispatcher controller
+ *
+ * PHP version 5
+ *
+ * @group dispatcher
+ *
+ * @category  Dispatcher
+ * @package   Dispatcher
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dispatcher/tree/master/classes/charougna/dispatcher.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Charougna_Dispatcher controller
+ *
+ * PHP version 5
+ *
+ * @group dispatcher
+ *
+ * @category  Dispatcher
+ * @package   Dispatcher
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dispatcher/tree/master/classes/charougna/dispatcher.php
+ */
+class Controller_Charougna_Dispatcher extends Controller
+{
+
+} // End class Charougna_Dispatcher
---
+++ classes/controller/dispatcher.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Dispatcher controller
+ *
+ * PHP version 5
+ *
+ * @group dispatcher
+ *
+ * @category  Dispatcher
+ * @package   Dispatcher
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dispatcher/tree/master/classes/dispatcher.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Dispatcher controller
+ *
+ * PHP version 5
+ *
+ * @group dispatcher
+ *
+ * @category  Dispatcher
+ * @package   Dispatcher
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dispatcher/tree/master/classes/dispatcher.php
+ */
+class Controller_Dispatcher extends Controller_Charougna_Dispatcher
+{
+}
